fedc90376b1f6859f160fac593be3e3423ee24d7
Edgar <legranddidous@gmail.com>
2016-08-30 12:12:09 +0200

    Creando plugins Change Screen

---
+++ change_screen.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: Change Screen
+ * Plugin URI:  https://tommcfarlin.com
+ * Description: Many versions or styles of screen in your blog
+ * Version:     1.0.0
+ * Author:      Edgar Naranjo
+ * Author URI:  https://www.linkedin.com/in/edgar-naranjo-fuentes-747399121
+ * License:     GPL-2.0+
+
+{Change Screen} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+{Change Screen} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {https://www.gnu.org/licenses/old-licenses/gpl-2.0.html}.
+*/
+
+
+// bloqueo del acceso directo a los archivos del plugin
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
